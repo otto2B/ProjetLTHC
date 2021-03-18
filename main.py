877c1edf8539ef1e91a9b8579d2d0adf687adb41
otto2B <constantin.debentzmann@epfl.ch>
2021-03-18 09:50:30 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,35 @@
 import torch
 import tensorflow as tf
+import numpy
+
+#Size of the matrix Y
+N = 600
+M = N
+dim = 1
+
+#
+lambda_ = 1
+
+#temperatures
+beta_u = 1
+beta_v = beta_u
+
+#learning rates
+lambda_1 = 1
+lambda_2 = lambda_1
+
+
+# genere une matrice sur l'hypersphere
+
+
+def generateVector():
+	norm = np.random.normal
+	normal_deviates = norm(size=(dim, N))
+
+	radius = np.sqrt((normal_deviates**2).sum(axis=0))
+	y = normal_deviates/radius
+	return y
+
+
+
+
